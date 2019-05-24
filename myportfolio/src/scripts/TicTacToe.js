440ee9bf343a97acc3016cc39e686eb7311aa8f5
pmzarate <patriciamarakzarate@yahoo.com>
2019-05-23 22:30:00 -0500

    added function to TicTacToe

--- myportfolio/src/scripts/TicTacToe.js
+++ myportfolio/src/scripts/TicTacToe.js
@@ -1,38 +1,35 @@
-var gameMarker
+var gameMarker = "X";
 
-document.getElementById("box1").addEventListener("click",placeMark)
+// document.getElementById("box1").addEventListener("click",placeMark)
 
-document.getElementById("box2").addEventListener("click",placeMark)
+// document.getElementById("box2").addEventListener("click",placeMark)
 
-document.getElementById("box3").addEventListener("click",placeMark)
+// document.getElementById("box3").addEventListener("click",placeMark)
 
-document.getElementById("box4").addEventListener("click",placeMark)
+// document.getElementById("box4").addEventListener("click",placeMark)
 
-document.getElementById("box5").addEventListener("click",placeMark)
+// document.getElementById("box5").addEventListener("click",placeMark)
 
-document.getElementById("box6").addEventListener("click",placeMark)
+// document.getElementById("box6").addEventListener("click",placeMark)
 
-document.getElementById("box7").addEventListener("click",placeMark)
+// document.getElementById("box7").addEventListener("click",placeMark)
 
-document.getElementById("box8").addEventListener("click",placeMark)
+// document.getElementById("box8").addEventListener("click",placeMark)
 
-document.getElementById("box9").addEventListener("click",placeMark)
-
-
-
+// document.getElementById("box9").addEventListener("click",placeMark)
 
 function changeMarkerToX() {
-    gameMarker = "X"
-    console.log ("click")
+	gameMarker = "X";
+	console.log("click");
 }
 
-function changeMarkerToO(){
-    gameMarker = "0"
-    console.log ("click")
+function changeMarkerToO() {
+	gameMarker = "O";
+	console.log("click");
 }
 
-function placeMark(){ 
-    .innerHTML = 
-    console.log ("you did it")
+function placeMark(myId) {
+	document.getElementById(myId).innerHTML = gameMarker;
+	console.log("you did it");
 }
 
